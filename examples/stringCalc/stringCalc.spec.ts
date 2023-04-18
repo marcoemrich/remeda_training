40df9ebee94c7cab2228b7982c5e971a3d3e30b8
Marco Emrich <marco.emrich@codecentric.de>
2023-04-18 13:56:34 +0200

    uglified classic example

--- examples/stringCalc/stringCalc.spec.ts
+++ examples/stringCalc/stringCalc.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { stringCalc } from "./stringCalc_remeda";
+import { stringCalc } from "./stringCalc_classic";
 
 describe("stringCalc", () => {
   it("should return 0 for an empty string", () => {
--- examples/stringCalc/stringCalc_classic.ts
+++ examples/stringCalc/stringCalc_classic.ts
@@ -1,7 +1,20 @@
 export const stringCalc = (str: string) => {
   let result = 0;
+  let part = "";
+  let parts: string[] = [];
+  let partNr = 0;
 
-  const parts = str.split(",");
+  for (let i = 0; i < str.length; ++i) {
+    if (str[i] != ",") {
+      part += str[i];
+    } else {
+      parts[++partNr] = part;
+      part = "";
+    }
+  }
+
+  parts[++partNr] = part;
+
   for (let i = 0; i < parts.length; ++i) {
     const n = Number(parts[i]);
     if (n < 1000) {
